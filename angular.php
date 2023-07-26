78372799c2cb3770fe0ca041b9e60c8d75d8a28a
gituserYun <123449464+gituserYun@users.noreply.github.com>
2023-07-27 03:00:13 +0900

    Delete angular.php

--- angular.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html ng-app>
-  <head>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
-  </head>
-  <body>
-      <?php echo htmlspecialchars($_GET['msg']); ?>
-  </body>
-</html>
