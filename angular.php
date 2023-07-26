a5238eb690cc8f2fc259cf0a08a798c78906f12a
gituserYun <123449464+gituserYun@users.noreply.github.com>
2023-07-27 00:57:32 +0900

    Update and rename AngularJS.html to angular.php

--- angular.php
+++ angular.php
@@ -4,11 +4,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
   </head>
   <body>
-    <div>
-      <label>Name:</label>
-      <input type="text" ng-model="yourName" placeholder="Enter a name here">
-      <hr>
-      <h1>Hello {{yourName}}!</h1>
-    </div>
+      <?php echo htmlspecialchars($_GET['msg']); ?>
   </body>
 </html>
