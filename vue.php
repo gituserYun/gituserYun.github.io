f4965b33f52bade578126a3eec69b046020058e5
gituserYun <123449464+gituserYun@users.noreply.github.com>
2023-07-27 00:38:05 +0900

    Update vue.php

--- vue.php
+++ vue.php
@@ -1,9 +1,18 @@
-<script src="https://unpkg.com/vue@3"></script>
-<div id="app">
-	<h1>Hello ?name=${escapeHTML(name)}</h1>
-</div>
-<script>
-  new Vue({
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <script src="https://unpkg.com/vue@3"></script>
+  <title>TEST SERVER</title>
+</head>
+<body>
+  <div id="app">
+      <h1>Hello ?name=${escapeHTML(name)}</h1>
+  </div>
+  <script>	  
+    new Vue({
     el: '#app'
   });
-</script>
+  </script>
+</body>
+</html>
